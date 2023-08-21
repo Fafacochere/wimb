4e672fcd117ac2fb45b6f8dbdbff3e149a84ac43
Fafacochere <fraulline@gmail.com>
2023-08-21 13:33:59 +0200

    change default text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <h1 className="text-7xl">Coucou ma chérie !</h1>
+      <h1 className="text-7xl">Maintenance en cours</h1>
     </div>
   );
 };
