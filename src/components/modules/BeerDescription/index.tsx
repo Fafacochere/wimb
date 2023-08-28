79b5e9ff59413236c7edc0ea35e62fa7dd0bf075
Fafacochere <fraulline@gmail.com>
2023-08-28 13:53:03 +0200

    some adjustments

--- src/components/modules/BeerDescription/index.tsx
+++ src/components/modules/BeerDescription/index.tsx
@@ -3,16 +3,25 @@ export default function BeerDescription() {
         <div className="flex items-center space-x-4 border-gray-300">
             <img className="w-16 h-16 rounded-md" src="https://images.unsplash.com/photo-1608742598121-15038a8db33c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80" alt="Biere 1" />
             <div>
-                <h3 className="text-lg font-semibold">Ma bière sélectionée 1</h3>
-                <p className="">Description de la bière que vous avez sélectionnée</p>
+                <h3 className="text-lg font-semibold leading-5">Ma bière sélectionée 1</h3>
+                <p className="leading-5 text-sm">Description de la bière que vous avez sélectionnée</p>
             </div>
         </div>
-        <div className="mt-1">
+        <div className="pt-3 pb-2">
             <p className="font-bold">Caractéristiques:</p>
-            <ul className="list-none">
-                <li>Type: Ambrée</li>
-                <li>Intensité: 4,7%</li>
-                <li>Origine: Belgique</li>
+            <ul className="list-none w-full">
+                <li className="flex justify-between pr-1">
+                    <div>Type :</div>
+                    <div className="text-gray-500">Ambrée</div>
+                </li>
+                <li className="flex justify-between pr-1">
+                    <div>Intensité :</div>
+                    <div className="text-gray-500">4.7%</div>
+                </li>
+                <li className="flex justify-between pr-1">
+                    <div>Origine :</div>
+                    <div className="text-gray-500">Belgique</div>
+                </li>
             </ul>
         </div>
     </div>
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 export const Input = (props: HTMLProps<HTMLInputElement>) => {
 
     const classes = classNames(
-        'border-1 border-gray-700 rounded-md px-3 py-2 w-[350px] max-w-full shadow-md',
+        'border-[1px] border-gray-500 rounded-md px-3 py-2 w-[350px] max-w-full shadow-lg',
         props.className
     )
     const { className, ...rest } = props;
